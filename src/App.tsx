2b43946f3a01f171b934e59f1e0ab6fda53fae12
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2025-05-28 18:29:04 +0330

    build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   const showAside = useBreakpointValue({ base: false, lg: true });
@@ -7,8 +8,8 @@ function App() {
       <Grid
         templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
       >
-        <GridItem area="nav" bg="coral">
-          Nav
+        <GridItem area="nav">
+          <NavBar />
         </GridItem>
         {showAside && (
           <GridItem area="aside" bg="gold">
