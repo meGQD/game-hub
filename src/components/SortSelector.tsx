a13a12b664a6451ce87baabc9b0adbe636610568
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2025-06-17 15:50:36 +0330

    Sorting games added

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,24 +1,45 @@
 import { Button, Menu, Portal } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  selectedSortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, selectedSortOrder }: Props) => {
+  const sortOrders = [
+    { value: "", label: "Relevance" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Released date" },
+    { value: "-added", label: "Date added" },
+    { value: "-rating", label: "Average rating" },
+    { value: "-metacritic", label: "Popularity" },
+  ];
+
+  const currentSortOrder = sortOrders.find(
+    (order) => order.value === selectedSortOrder
+  );
+
   return (
     <>
       <Menu.Root>
         <Menu.Trigger asChild>
           <Button variant="outline" size="md">
-            Order by: Relevance <BsChevronDown />
+            Order by: {currentSortOrder?.label || "Relevance"} <BsChevronDown />
           </Button>
         </Menu.Trigger>
         <Portal>
           <Menu.Positioner>
             <Menu.Content>
-              <Menu.Item value="Relevance">Relevance</Menu.Item>
-              <Menu.Item value="Date added">Date added</Menu.Item>
-              <Menu.Item value="Name">Name</Menu.Item>
-              <Menu.Item value="Release date">Release date</Menu.Item>
-              <Menu.Item value="Popularity">Popularity</Menu.Item>
-              <Menu.Item value="Average rating">Average rating</Menu.Item>
+              {sortOrders.map((order) => (
+                <Menu.Item
+                  onClick={() => onSelectSortOrder(order.value)}
+                  key={order.value}
+                  value={order.value}
+                >
+                  {order.label}
+                </Menu.Item>
+              ))}
             </Menu.Content>
           </Menu.Positioner>
         </Portal>
